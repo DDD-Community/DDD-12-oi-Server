0572f075493e2b7c8e7943adabfcc268cfe6b833
sengjun0624 <sengjun0624@naver.com>
2025-06-11 17:46:56 +0900

    :fire: delete: ScheduleSpot 도메인 제거

--- src/main/java/com/ddd/oi/schedule_tag/repository/ScheduleTagRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ddd.oi.schedule_tag.repository;
-import com.ddd.oi.schedule_tag.domain.ScheduleTag;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface ScheduleTagRepository extends JpaRepository<ScheduleTag, Long> {
-}
