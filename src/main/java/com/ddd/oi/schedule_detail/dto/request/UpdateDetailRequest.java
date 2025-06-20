50b5c6b5a5a9d5faab4fbe9c0383660f1696e5ed
sengjun0624 <sengjun0624@naver.com>
2025-06-20 16:25:18 +0900

    🔧 chore: 스웨거 디폴트 값 추가

--- src/main/java/com/ddd/oi/schedule_detail/dto/request/UpdateDetailRequest.java
+++ src/main/java/com/ddd/oi/schedule_detail/dto/request/UpdateDetailRequest.java
@@ -1,26 +1,21 @@
 package com.ddd.oi.schedule_detail.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record UpdateDetailRequest(
-	@NotNull
-	@JsonFormat(pattern = "HH:mm")
-	LocalTime startTime,
+		@NotNull @JsonFormat(pattern = "HH:mm") @Schema(type = "string", format = "time", pattern = "HH:mm", example = "14:30", description = "시작 시간") LocalTime startTime,
 
-	@NotNull
-	LocalDate targetDate,
+		@NotNull @Schema(description = "날짜", example = "2025-07-01") LocalDate targetDate,
 
-	String memo,
+		@Schema(description = "메모", example = "아침 식사 후 출발") String memo,
 
-	@NotNull
-	String spotName,
+		@NotNull @Schema(description = "장소명", example = "서울역") String spotName,
 
-	@NotNull
-	Double latitude,
+		@NotNull @Schema(description = "위도", example = "37.554722") Double latitude,
 
-	@NotNull
-	Double longitude
-) {}
+		@NotNull @Schema(description = "경도", example = "126.970833") Double longitude) {
+}
