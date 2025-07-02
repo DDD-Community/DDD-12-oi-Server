fb9bfa9d800a2b15b16535df11eb6ab8efb9e48d
sengjun0624 <sengjun0624@naver.com>
2025-07-02 16:32:40 +0900

    :hammer: chore: dto 스웨거 어노테이션 삭제

--- src/main/java/com/ddd/oi/schedule_detail/dto/request/CreateDetailRequest.java
+++ src/main/java/com/ddd/oi/schedule_detail/dto/request/CreateDetailRequest.java
@@ -9,15 +9,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateDetailRequest(
-		@NotNull @JsonFormat(pattern = "HH:mm") @Schema(type = "string", format = "time", pattern = "HH:mm", example = "14:30", description = "시작 시간") LocalTime startTime,
+		@NotNull @JsonFormat(pattern = "HH:mm") LocalTime startTime,
 
-		@Schema(description = "메모", example = "아침 식사 후 출발") String memo,
+		 String memo,
 
-		@NotNull @Schema(description = "날짜", example = "2025-07-01") LocalDate targetDate,
+		@NotNull  LocalDate targetDate,
 
-		@NotNull @Schema(description = "장소명", example = "서울역") String spotName,
+		@NotNull String spotName,
 
-		@NotNull @Schema(description = "위도", example = "37.554722") Double latitude,
+		@NotNull  Double latitude,
 
-		@NotNull @Schema(description = "경도", example = "126.970833") Double longitude) {
+		@NotNull Double longitude) {
 }
--- src/main/java/com/ddd/oi/schedule_detail/dto/request/UpdateDetailRequest.java
+++ src/main/java/com/ddd/oi/schedule_detail/dto/request/UpdateDetailRequest.java
@@ -1,21 +1,23 @@
 package com.ddd.oi.schedule_detail.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record UpdateDetailRequest(
-		@NotNull @JsonFormat(pattern = "HH:mm") @Schema(type = "string", format = "time", pattern = "HH:mm", example = "14:30", description = "시작 시간") LocalTime startTime,
+	@NotNull @JsonFormat(pattern = "HH:mm") LocalTime startTime,
 
-		@NotNull @Schema(description = "날짜", example = "2025-07-01") LocalDate targetDate,
+	@NotNull LocalDate targetDate,
 
-		@Schema(description = "메모", example = "아침 식사 후 출발") String memo,
+	String memo,
 
-		@NotNull @Schema(description = "장소명", example = "서울역") String spotName,
+	@NotNull String spotName,
 
-		@NotNull @Schema(description = "위도", example = "37.554722") Double latitude,
+	@NotNull Double latitude,
 
-		@NotNull @Schema(description = "경도", example = "126.970833") Double longitude) {
+	@NotNull Double longitude) {
 }
