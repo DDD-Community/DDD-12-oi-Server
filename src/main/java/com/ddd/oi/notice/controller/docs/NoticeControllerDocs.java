6f5f79aca873239e572dd98223702f1ba263dc4c
sengjun0624 <sengjun0624@naver.com>
2025-08-09 15:25:26 +0900

    :recycle: refactor: Docs Pageable 응답 예시 수정 #65

--- src/main/java/com/ddd/oi/notice/controller/docs/NoticeControllerDocs.java
+++ src/main/java/com/ddd/oi/notice/controller/docs/NoticeControllerDocs.java
@@ -28,10 +28,18 @@ public interface NoticeControllerDocs {
 	CustomApiResponse<NoticeResponse> updateNotice(@PathVariable Long id,
 		@RequestBody @Valid NoticeUpdateRequest request);
 
-	@Operation(summary = "공지사항 목록 조회", description = "공지사항 목록을 페이지네이션으로 조회합니다. \n  정렬기준은 updatedAt을 넣어주세요.", parameters = {
-		@Parameter(name = "page", description = "페이지 번호 (0부터 시작)", example = "0"),
-		@Parameter(name = "size", description = "페이지 크기", example = "8"),
-		@Parameter(name = "sort", description = "정렬 기준", example = "updatedAt")}, responses = @ApiResponse(responseCode = "200", description = "공지사항 목록 조회 성공", content = @Content(mediaType = "application/json", examples = @ExampleObject(name = "성공 응답 예시", value = "{\n  \"statusCode\": 200,\n  \"resultType\": \"SUCCESS\",\n  \"data\": {\n    \"content\": [\n      {\n        \"id\": 1,\n        \"title\": \"공지사항 제목\",\n        \"content\": \"공지사항 내용\",\n        \"updatedAt\": \"2023-10-01T12:00:00\"\n      }\n    ],\n    \"pageable\": {\n      \"pageNumber\": 0,\n      \"pageSize\": 10\n    }\n  },\n  \"message\": \"공지사항 목록 조회 성공\"\n}"))))
+	@Operation(summary = "공지사항 목록 조회", description = "공지사항 목록을 페이지네이션으로 조회합니다.", responses = @ApiResponse(responseCode = "200", description = "공지사항 목록 조회 성공", content = @Content(mediaType = "application/json", examples = @ExampleObject(name = "성공 응답 예시", value =
+		"{\n" + "  \"statusCode\": 200,\n" + "  \"resultType\": \"SUCCESS\",\n" + "  \"data\": {\n"
+			+ "    \"content\": [\n" + "      {\n" + "        \"title\": \"공지사항 제목\",\n"
+			+ "        \"content\": \"공지사항 내용\",\n" + "        \"updatedAt\": \"2023-10-01T12:00:00\"\n" + "      }\n"
+			+ "    ],\n" + "    \"pageable\": {\n" + "      \"pageNumber\": 0,\n" + "      \"pageSize\": 1,\n"
+			+ "      \"sort\": {\n" + "        \"empty\": true,\n" + "        \"sorted\": false,\n"
+			+ "        \"unsorted\": true\n" + "      },\n" + "      \"offset\": 0,\n" + "      \"paged\": true,\n"
+			+ "      \"unpaged\": false\n" + "    },\n" + "    \"last\": true,\n" + "    \"totalPages\": 1,\n"
+			+ "    \"totalElements\": 1,\n" + "    \"size\": 1,\n" + "    \"sort\": {\n" + "      \"empty\": true,\n"
+			+ "      \"sorted\": false,\n" + "      \"unsorted\": true\n" + "    },\n" + "    \"number\": 0,\n"
+			+ "    \"first\": true,\n" + "    \"numberOfElements\": 1,\n" + "    \"empty\": false\n" + "  },\n"
+			+ "  \"message\": \"공지사항 목록 조회 성공\"\n" + "}"))))
 	CustomApiResponse<Page<NoticeResponse>> getNotices(Pageable pageable);
 
 	@Operation(summary = "공지사항 조회", description = "공지사항 ID를 통해 공지사항을 조회합니다.", parameters = @Parameter(name = "id", description = "공지사항 ID", example = "1"), responses = @ApiResponse(responseCode = "200", description = "공지사항 조회 성공", content = @Content(mediaType = "application/json", examples = @ExampleObject(name = "성공 응답 예시", value = "{\n  \"statusCode\": 200,\n  \"resultType\": \"SUCCESS\",\n  \"data\": {\n    \"id\": 1,\n    \"title\": \"공지사항 제목\",\n    \"content\": \"공지사항 내용\",\n    \"updatedAt\": \"2023-10-01T12:00:00\"\n  },\n  \"message\": \"공지사항 조회 성공\"\n}"))))
