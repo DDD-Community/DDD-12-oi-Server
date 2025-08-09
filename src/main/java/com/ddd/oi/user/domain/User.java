c28ea2357241ebf0293c4fdb9bd92981539f5ff4
sengjun0624 <sengjun0624@naver.com>
2025-08-09 14:25:44 +0900

    :recycle: refactor: User 테이블에 lastReadAt 컬럼 추가 #65

--- src/main/java/com/ddd/oi/user/domain/User.java
+++ src/main/java/com/ddd/oi/user/domain/User.java
@@ -1,5 +1,7 @@
 package com.ddd.oi.user.domain;
 
+import java.time.LocalDateTime;
+
 import com.ddd.oi.common.domain.BaseEntity;
 
 import jakarta.persistence.*;
@@ -38,6 +40,11 @@ public class User extends BaseEntity {
 	@Enumerated(EnumType.STRING)
 	private RoleType role;
 
+	@Column(name = "last_read_at")
+	private LocalDateTime lastReadAt;
+	public void updateLastReadAt(LocalDateTime lastReadAt) {
+		this.lastReadAt = lastReadAt;
+	}
 	public void updateNickname(String nickname) {
 		this.nickname = nickname;
 	}
