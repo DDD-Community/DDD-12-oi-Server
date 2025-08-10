160e7c18b1cd1793c44248c87fe86b73f69766bf
이승준 <105282117+sengjun0624@users.noreply.github.com>
2025-08-10 20:04:36 +0900

    :recycle: refactor: 컨텐츠 조회 날짜구간 제거 (#67)

--- src/main/java/com/ddd/oi/contents/controller/ContentsController.java
+++ src/main/java/com/ddd/oi/contents/controller/ContentsController.java
@@ -44,16 +44,9 @@ public class ContentsController implements ContentsControllerDocs {
 		return CustomApiResponse.success(contentsId, 200, "컨텐츠 삭제 성공");
 	}
 
-/*	@GetMapping
+	@GetMapping
 	@Override
 	public CustomApiResponse<List<ContentsResponse>> getContentsList() {
 		return CustomApiResponse.success(contentsService.getContentsWithImagesAndSpots(), 200, "컨텐츠 리스트 조회 성공");
-	}*/
-	@GetMapping
-	@Override
-	public CustomApiResponse<List<ContentsResponse>> getContentsByDateRange(
-		@RequestParam LocalDate fromDate,
-		@RequestParam LocalDate toDate) {
-		return CustomApiResponse.success(contentsService.getContentsByDateRange(fromDate, toDate), 200, "컨텐츠 날짜별 조회 성공");
 	}
 }
