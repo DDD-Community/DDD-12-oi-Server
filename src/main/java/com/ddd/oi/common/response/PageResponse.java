63be2c9dc301163ae3bbe408557cdd515fd0066f
이승준 <105282117+sengjun0624@users.noreply.github.com>
2025-08-16 14:50:54 +0900

    ✨  Feat: 페이지네이션 공통응답 객체 추가, 스케줄 디테일 Docs 분리 (#77)
    * :sparkles: feat: Pagination 공통 응답코드 추가 #76
    
    * :sparkles: feat: Notice Pagination 공통 응답객체 적용 #76
    
    * :sparkles: feat: FAQ Pagination 공통 응답객 적용 #76
    
    * :recycle: refactor: Docs 분리 #76

---
+++ src/main/java/com/ddd/oi/common/response/PageResponse.java
@@ -0,0 +1,25 @@
+package com.ddd.oi.common.response;
+
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+public class PageResponse<T> {
+    private final List<T> content;
+    private final int pageNumber;
+    private final int pageSize;
+    private final long totalElements;
+    private final int totalPages;
+    private final boolean hasNext;
+
+    public PageResponse(Page<T> page) {
+        this.content = page.getContent();
+        this.pageNumber = page.getNumber();
+        this.pageSize = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.hasNext = page.hasNext();
+    }
+}
--- src/main/java/com/ddd/oi/faq/controller/FaqController.java
+++ src/main/java/com/ddd/oi/faq/controller/FaqController.java
@@ -1,13 +1,17 @@
 package com.ddd.oi.faq.controller;
 
 import com.ddd.oi.common.response.CustomApiResponse;
+import com.ddd.oi.common.response.PageResponse;
 import com.ddd.oi.faq.controller.docs.FaqControllerDocs;
 import com.ddd.oi.faq.dto.FaqCreateRequest;
 import com.ddd.oi.faq.dto.FaqResponse;
 import com.ddd.oi.faq.dto.FaqUpdateRequest;
 import com.ddd.oi.faq.service.FaqService;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,31 +20,34 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class FaqController implements FaqControllerDocs {
 
-    private final FaqService faqService;
+	private final FaqService faqService;
 
-    @PostMapping
-    public CustomApiResponse<FaqResponse> createFaq(@RequestBody FaqCreateRequest request) {
-        return CustomApiResponse.success(faqService.createFaq(request), 200, "FAQ 생성 성공");
-    }
+	@PostMapping
+	public CustomApiResponse<FaqResponse> createFaq(@RequestBody FaqCreateRequest request) {
+		return CustomApiResponse.success(faqService.createFaq(request), 200, "FAQ 생성 성공");
+	}
 
-    @GetMapping("/{id}")
-    public CustomApiResponse<FaqResponse> getFaq(@PathVariable Long id) {
-        return CustomApiResponse.success(faqService.getFaq(id), 200, "FAQ 조회 성공");
-    }
+	@GetMapping("/{id}")
+	public CustomApiResponse<FaqResponse> getFaq(@PathVariable Long id) {
+		return CustomApiResponse.success(faqService.getFaq(id), 200, "FAQ 조회 성공");
+	}
 
-    @PutMapping("/{id}")
-    public CustomApiResponse<FaqResponse> updateFaq(@PathVariable Long id, @RequestBody FaqUpdateRequest request) {
-        return CustomApiResponse.success(faqService.updateFaq(id, request), 200, "FAQ 수정 성공");
-    }
+	@PutMapping("/{id}")
+	public CustomApiResponse<FaqResponse> updateFaq(@PathVariable Long id, @RequestBody FaqUpdateRequest request) {
+		return CustomApiResponse.success(faqService.updateFaq(id, request), 200, "FAQ 수정 성공");
+	}
 
-    @DeleteMapping("/{id}")
-    public CustomApiResponse<Long> deleteFaq(@PathVariable Long id) {
-        faqService.deleteFaq(id);
-        return CustomApiResponse.success(id, 200, "FAQ 삭제 성공");
-    }
+	@DeleteMapping("/{id}")
+	public CustomApiResponse<Long> deleteFaq(@PathVariable Long id) {
+		faqService.deleteFaq(id);
+		return CustomApiResponse.success(id, 200, "FAQ 삭제 성공");
+	}
 
-    @GetMapping
-    public CustomApiResponse<Page<FaqResponse>> getFaqs(Pageable pageable) {
-        return CustomApiResponse.success(faqService.getFaqs(pageable), 200, "FAQ 목록 조회 성공");
-    }
+	@GetMapping
+	public CustomApiResponse<PageResponse<FaqResponse>> getFaqs(
+		@RequestParam(value = "pageNumber", defaultValue = "0") int pageNumber,
+		@RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		return CustomApiResponse.success(faqService.getFaqs(pageable), 200, "FAQ 목록 조회 성공");
+	}
 }
