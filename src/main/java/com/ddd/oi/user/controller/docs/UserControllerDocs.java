f17de4806151eace7233130df0e17009b4499f60
sengjun0624 <sengjun0624@naver.com>
2025-08-25 19:54:18 +0900

    :recycle: refactor:  UserApi에 Spring Security 적용 #86

--- src/main/java/com/ddd/oi/user/controller/docs/UserControllerDocs.java
+++ src/main/java/com/ddd/oi/user/controller/docs/UserControllerDocs.java
@@ -5,27 +5,30 @@ import com.ddd.oi.user.domain.User;
 import com.ddd.oi.user.dto.request.UpdateNicknameRequest;
 import com.ddd.oi.user.dto.response.ShowUserResponse;
 import com.ddd.oi.user.dto.response.UpdateNicknameResponse;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "유저 컨트롤러", description = "유저 관련 API입니다.")
 public interface UserControllerDocs {
-    @Operation(summary = "유저 탈퇴", description = "유저 탈퇴 API")
-    CustomApiResponse<?> deleteUser(
-            @AuthenticationPrincipal User user,
-            @RequestHeader("Oauth-Authorization") String oauthAccessToken
-    );
-    @Operation(summary = "유저 닉네임 수정", description = "유저 닉네임 수정 API")
-    CustomApiResponse<UpdateNicknameResponse> updateUserNickname(
-            @AuthenticationPrincipal User user,
-            @Valid UpdateNicknameRequest request
-    );
+	@Operation(summary = "유저 탈퇴", description = "유저 탈퇴 API")
+	CustomApiResponse<?> deleteUser(
+		@AuthenticationPrincipal User user,
+		@RequestHeader("Oauth-Authorization") String oauthAccessToken
+	);
 
-    @Operation(summary = "유저 정보 조회/시스템 정보 조회",description = "유저 정보 조회/시스템 정보 조회 API")
-    CustomApiResponse<ShowUserResponse> showUserAndSystemInfo(
-            @RequestHeader("user-no") Long userId
-    );
+	@Operation(summary = "유저 닉네임 수정", description = "유저 닉네임 수정 API")
+	CustomApiResponse<UpdateNicknameResponse> updateUserNickname(
+		@AuthenticationPrincipal User user,
+		@Valid UpdateNicknameRequest request
+	);
+
+	@Operation(summary = "유저 정보 조회/시스템 정보 조회", description = "유저 정보 조회/시스템 정보 조회 API")
+	CustomApiResponse<ShowUserResponse> showUserAndSystemInfo(
+		@AuthenticationPrincipal User user
+	);
 }
