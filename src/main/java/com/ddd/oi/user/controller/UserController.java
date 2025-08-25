aa3ff46f791a47ab6427e03ec09f459071570c19
이승준 <105282117+sengjun0624@users.noreply.github.com>
2025-08-25 20:00:38 +0900

    ✨ Feat: Security 적용 (#87)
    * :recycle: refactor:  Security 적용 #83
    
    * :recycle: refactor:  UserApi에 Spring Security 적용 #86
    
    * :recycle: refactor: ScheduleApi에 Spring Security 적용 #86

--- src/main/java/com/ddd/oi/user/controller/UserController.java
+++ src/main/java/com/ddd/oi/user/controller/UserController.java
@@ -7,8 +7,10 @@ import com.ddd.oi.user.dto.request.UpdateNicknameRequest;
 import com.ddd.oi.user.dto.response.ShowUserResponse;
 import com.ddd.oi.user.dto.response.UpdateNicknameResponse;
 import com.ddd.oi.user.service.UserService;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,37 +24,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController implements UserControllerDocs {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    @DeleteMapping
-    @Override
-    public CustomApiResponse<?> deleteUser(
-            @AuthenticationPrincipal User user,
-            @RequestHeader("Oauth-Authorization") String oauthAccessToken
-    ) {
-        Boolean result = userService.deleteUser(user, oauthAccessToken);
-        return CustomApiResponse.success(result, 200, "유저 탈퇴 성공");
-    }
+	@DeleteMapping
+	@Override
+	public CustomApiResponse<?> deleteUser(
+		@AuthenticationPrincipal User user,
+		@RequestHeader("Oauth-Authorization") String oauthAccessToken
+	) {
+		Boolean result = userService.deleteUser(user, oauthAccessToken);
+		return CustomApiResponse.success(result, 200, "유저 탈퇴 성공");
+	}
 
-    @PatchMapping
-    @Override
-    public CustomApiResponse<UpdateNicknameResponse> updateUserNickname(
-            @AuthenticationPrincipal User user,
-            @Valid UpdateNicknameRequest request
-    ) {
-        UpdateNicknameResponse result = userService.updateNickname(user,request);
-        return CustomApiResponse.success(result,200,"유저 닉네임 수정 성공");
-    }
+	@PatchMapping
+	@Override
+	public CustomApiResponse<UpdateNicknameResponse> updateUserNickname(
+		@AuthenticationPrincipal User user,
+		@Valid UpdateNicknameRequest request
+	) {
+		UpdateNicknameResponse result = userService.updateNickname(user, request);
+		return CustomApiResponse.success(result, 200, "유저 닉네임 수정 성공");
+	}
 
-
-    @GetMapping
-    @Override
-    public CustomApiResponse<ShowUserResponse> showUserAndSystemInfo(
-            @RequestHeader("user-no") Long userId
-    ) {
-        ShowUserResponse result = userService.showUserAndSystemInfo(userId);
-        return CustomApiResponse.success(result,200,"유저 정보 조회/시스템 정보 조회 성공");
-    }
-
+	@GetMapping
+	@Override
+	public CustomApiResponse<ShowUserResponse> showUserAndSystemInfo(
+		@AuthenticationPrincipal User user
+	) {
+		ShowUserResponse result = userService.showUserAndSystemInfo(user.getId());
+		return CustomApiResponse.success(result, 200, "유저 정보 조회/시스템 정보 조회 성공");
+	}
 
 }
