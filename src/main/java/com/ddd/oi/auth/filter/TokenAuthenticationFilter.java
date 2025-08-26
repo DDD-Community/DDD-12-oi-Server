62c9ad38410487670c04d4dc388fb61d692494e9
bangyewon <90364660+bangyewon@users.noreply.github.com>
2025-08-26 14:06:32 +0900

    :bug: hotfix: 분기별 Filter jwt파싱 수정 #88

--- src/main/java/com/ddd/oi/auth/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/ddd/oi/auth/filter/TokenAuthenticationFilter.java
@@ -3,13 +3,13 @@ package com.ddd.oi.auth.filter;
 import com.ddd.oi.common.exception.OiException;
 import com.ddd.oi.common.response.ErrorCode;
 import com.ddd.oi.common.utils.JWTUtil;
+import com.ddd.oi.user.domain.ProviderInfo;
 import com.ddd.oi.user.domain.User;
 import com.ddd.oi.user.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,6 +18,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
@@ -32,7 +33,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         String path = request.getRequestURI();
-        if (path.startsWith("/api/v1/auth/**")) {
+        if (path.startsWith("/api/v1/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -41,11 +42,18 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
         if (token != null && !jwtUtil.isExpired(token)) {
             String email = jwtUtil.getEmail(token);
+            String providerId = jwtUtil.getProviderId(token);
             String role = jwtUtil.getRole(token);
 
-            User user = userRepository.findByEmail(email)
-                    .orElseThrow(() -> new OiException(ErrorCode.ENTITY_NOT_FOUND));
+            User user;
 
+            if (providerId != null && email == null) {
+                user = userRepository.findByProviderInfoAndProviderId(ProviderInfo.KAKAO, providerId)
+                        .orElseThrow(() -> new OiException(ErrorCode.ENTITY_NOT_FOUND));
+            } else {
+                user = userRepository.findByEmail(email)
+                        .orElseThrow(() -> new OiException(ErrorCode.ENTITY_NOT_FOUND));
+            }
 
             UsernamePasswordAuthenticationToken authenticationToken =
                     new UsernamePasswordAuthenticationToken(
