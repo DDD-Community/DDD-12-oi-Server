d729ae33094bd4923d20fb66b59003caa3e97974
sengjun0624 <sengjun0624@naver.com>
2025-08-29 19:11:46 +0900

    🔒️ refactor: SecurityConfig /images 경로 열기  #86

--- src/main/java/com/ddd/oi/common/config/SecurityConfig.java
+++ src/main/java/com/ddd/oi/common/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
             "/swagger-resources/**",
             "/v3/api-docs/**",
             "/api/v1/auth/**",
-          //    "/api/v1/**"
+            "/api/v1/s3/images/**"
+             // "/api/v1/**"
     };
 
     @Bean
